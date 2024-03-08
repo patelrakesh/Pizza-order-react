4b9d9e78275ee5d5462b1779cdfc423b280495f7
Rakesh Patel <patevinod84@gmail.com>
2024-03-08 18:30:19 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Pizza-order-react
+
+## Getting Started with Create React App <a id="gettingStarted"></a>
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+To get started you need to:
+
+1. Clone the project
+2. npm install
+3. Install listed dependencies
+4. Use available scripts, like npm start
+
+
+## Available Scripts <a id="scripts"></a>
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.\
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.\
+Your app is ready to be deployed!
+
---
+++ src/components/footer/FooterMenu.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import ResetLocation from "../../helpers/ResetLocation";
+
+export default class FooterMenu extends React.Component {
+  render() {
+    return (
+      <ul className="footer-menu  flex-container flex-column">
+        <li>
+          <NavLink
+            style={({ isActive }) =>
+              isActive
+                ? {
+                  textDecoration: "none",
+                  color: "#ff6240",
+                }
+                : {}
+            }
+            onClick={ResetLocation}
+            className="txt-white"
+            to="/menu"
+          >
+            Menu
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink
+            style={({ isActive }) =>
+              isActive
+                ? {
+                  textDecoration: "none",
+                  color: "#ff6240",
+                }
+                : {}
+            }
+            onClick={ResetLocation}
+            className="txt-white"
+            to="/blog"
+          >
+            Blog
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            style={({ isActive }) =>
+              isActive
+                ? {
+                  textDecoration: "none",
+                  color: "#ff6240",
+                }
+                : {}
+            }
+            onClick={ResetLocation}
+            className="txt-white"
+            to="/about"
+          >
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            style={({ isActive }) =>
+              isActive
+                ? {
+                  textDecoration: "none",
+                  color: "#ff6240",
+                }
+                : {}
+            }
+            onClick={ResetLocation}
+            className="txt-white"
+            to="/contact"
+          >
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+    );
+  }
+}
